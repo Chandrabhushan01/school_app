04a90e4705748ead9f92342271805b0bd2c96e17
Chandrabhushan <cbdcool@gmail.com>
2019-12-02 19:14:47 +0530

    fixes

--- school/models.py
+++ school/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 
 class Subject(models.Model):
-    name = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    name = models.CharField(max_length=64, unique=True)
     chapters = models.IntegerField(default=0)
     total_duration_min = models.IntegerField(default=0)
     per_class_duration_min = models.IntegerField(default=0)
@@ -15,7 +15,7 @@ class Subject(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=64, null=True, blank=True)
+    name = models.CharField(max_length=64)
     doj = models.DateField(null=True, blank=True)
     salary = models.FloatField(default=0)
     web_lecture = models.BooleanField(default=False)
@@ -35,11 +35,11 @@ class ClassRoom(models.Model):
         (CANOPY, 'canopy'),
         (ELEVATED, 'elevated')
     )
-    name = models.CharField(max_length=64, unique=True, null=True, blank=True)
-    capacity = models.IntegerField(null=True, blank=True)
+    name = models.CharField(max_length=64, unique=True)
+    capacity = models.IntegerField(default=0)
     web_lecture_support = models.BooleanField(default=False)
     shape = models.CharField(
-        max_length=64, choices=SHAPE_OPTION, default=None, null=True, blank=True
+        max_length=64, choices=SHAPE_OPTION, default=None
     )
     subjects = models.ManyToManyField(Subject)
     teachers = models.ManyToManyField(Teacher)
@@ -48,11 +48,11 @@ class ClassRoom(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
 class Student(models.Model):
-    name = models.CharField(max_length=64, null=True, blank=True)
+    name = models.CharField(max_length=64)
     doj = models.DateField(null=True, blank=True)
     standard = models.IntegerField(null=True, blank=True)
     roll_no = models.AutoField(primary_key=True)
-    ranking = models.IntegerField(null=True, blank=True)
+    ranking = models.IntegerField(default=0)
     subjects = models.ManyToManyField(Subject)
     teachers = models.ManyToManyField(Teacher)
     is_deleted = models.BooleanField(default=False)
@@ -60,9 +60,9 @@ class Student(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
 class Relative(models.Model):
-    name = models.CharField(max_length=64, null=True, blank=True)
+    name = models.CharField(max_length=64)
     contact_number = models.CharField(max_length=10, null=True, blank=True)
-    relation = models.CharField(max_length=64, null=True, blank=True)
+    relation = models.CharField(max_length=64)
     students = models.ManyToManyField(Student)
     is_deleted = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
